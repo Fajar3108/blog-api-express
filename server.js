407d40b11ae73b5ed5d7b053c8546b9e9de2f4cd
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-07-07 11:46:26 +0700

    Add: Cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require('./database/conn');
 const express = require('express');
+const cors = require('cors');
 const { APP_PORT } = require('./config');
+
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.listen(APP_PORT, () => {
     console.log(`App listening on port http://localhost:${APP_PORT}`);
