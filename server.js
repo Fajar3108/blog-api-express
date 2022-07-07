46af75cb25de2a7d840a671bb0c8788eee9716a4
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-07-07 10:58:54 +0700

    Add: Setup Router

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 require('./database/conn');
+const express = require('express');
+const app = express();
+const port = process.env.APP_PORT;
+
+app.listen(port, () => {
+    console.log(`App listening on port http://localhost:${port}`);
+});
 
