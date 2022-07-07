a828adc963a857582b2e0c4ef8fae42f93f2c7c6
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-07-07 14:15:52 +0700

    Add: Update post by id

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -10,10 +10,22 @@ const PostController = {
         res.json(post);
     },
     store: async (req, res) => {
-        const post = new Post(req.body);
         try {
+            const post = new Post(req.body);
             await post.save();
             res.json({ message: 'Post created successfuly' });
+        } catch(error) {
+            res.status(400).json({ message: error.message });
+        }
+    },
+    update: async (req, res) => {
+        const post = Post.findOne({ _id: req.params.id });
+        if (!post) res.status(404).json({ message: 'Post not found' });
+        try {
+            await Post.updateOne({ _id: req.params.id }, {$set: req.body}, {
+                runValidators: true,
+            });
+            res.json({ message: 'Post updated successfuly' });
         } catch(error) {
             res.status(400).json({ message: error.message });
         }
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,5 +4,6 @@ const { PostController } = require('../../controllers');
 postRouter.get('/', PostController.index);
 postRouter.get('/:id', PostController.show);
 postRouter.post('/', PostController.store);
+postRouter.patch('/:id', PostController.update);
 
 module.exports = postRouter;
