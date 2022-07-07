7594cf9ca5c75c0c162ca9bc9d6f6f0ad6f51a88
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-07-07 13:47:28 +0700

    Add: Get Post by ID

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -5,6 +5,10 @@ const PostController = {
         const posts = await Post.find();
         res.json(posts);
     },
+    show: async (req, res) => {
+        const post = await Post.findOne({ _id: req.params.id })
+        res.json(post);
+    }
 };
 
 module.exports = PostController;
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,5 +2,6 @@ const postRouter = require('express').Router();
 const { PostController } = require('../../controllers');
 
 postRouter.get('/', PostController.index);
+postRouter.get('/:id', PostController.show);
 
 module.exports = postRouter;
