a9bc04fc7d82558b0bd3c7c39a18eaee6b132559
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-07-07 11:11:18 +0700

    Add: express.json() middleware

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.APP_PORT;
 
+app.use(express.json());
+
 app.listen(port, () => {
     console.log(`App listening on port http://localhost:${port}`);
 });
